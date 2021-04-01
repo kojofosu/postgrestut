256fd8315791336bb30f53b8ea2056e25fa4ee81
Kojo Fosu Bempa Edue <kojofosube@gmail.com>
2021-04-01 15:28:57 +0000

    project init with required dependencies

---
+++ src/main/java/com/mcdev/springpostgreschallenge/SpringpostgreschallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mcdev.springpostgreschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpostgreschallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpostgreschallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcdev/springpostgreschallenge/SpringpostgreschallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcdev.springpostgreschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpostgreschallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
